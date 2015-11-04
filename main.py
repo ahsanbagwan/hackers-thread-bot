feac428e0595e8f3a6f4f4fdc0a97e9b00524cd7
Ahsan Bagwan <ahsanbagwan@gmail.com>
2015-11-04 17:37:08 +0530

    main

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import praw
 from prawoauth2 import PrawOAuth2Mini
 
-
 reddit_client = praw.Reddit('Project by /u/weekerman')
 oauth_helper = PrawOAuth2Mini(reddit_client, app_key=app_key,app_secret=app_secret,access_token=access_token,
 	                          refresh_token=refresh_token, scopes=scopes)
 
 # login code goes here
 
+subreddit = reddit_client.get_subreddit(subreddit)
+comments = subreddit.get_comments()
+
+def main()
+  while True:
+         
